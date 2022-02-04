8381b5c7e3d4e4f4ca9c406a957c47dc2f75510c
davidcuisa <daalgocumon@gmail.com>
2022-02-03 23:01:23 -0500

    03022022

--- ejercicios/06.js
+++ ejercicios/06.js
@@ -1,3 +1,24 @@
+const boton = document.getElementById("boton").onmouseover;
+const imag = document.getElementById("imag").onclick;
+
+imag.style.visibility = "hidden";
+
+const event = () => {
+    if (imag.style.visibility == "hidden") {
+      imag.style.visibility = "visible";
+    } else {
+      imag.style.visibility = "hidden";
+    }
+  };
+
+  boton.onclick = 
+
+
+
+  login.onclick = function () {
+    iniciarSeson();
+  };
+  
 
 
     
